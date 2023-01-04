a0fff79678df3e197cca0289df6a92ebe27a65ce
Delphisss <Kong_led_@hotmail.com>
2023-01-04 12:09:43 +0700

    okay

---
+++ 9-1.1.cpp
@@ -0,0 +1,55 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+    char rank ;
+    cout << "Input your rank: ";
+    cin >> rank;
+    switch (rank){
+    case 'S':
+        cout << "You have received Super Ultra Rare Unit!!!\n";
+        cout << "You have received 5 gems.\n";
+        cout << "You have received 1 gems.\n";
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        break;
+    
+    case 'A':
+        cout << "You have received 5 gems.\n";
+        cout << "You have received 1 gems.\n";
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        break;
+
+    case 'B':
+        cout << "You have received 1 gems.\n";
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        break;
+
+    case 'C':
+        cout << "You have received 2000 coins.\n";
+        cout << "You have received very KAK items.\n";
+        break;
+
+    case 'D':
+        cout << "You have received very KAK items.\n";
+
+    default:
+        cout << "You KAK .\n";
+        break;
+    }
+
+  return 0;
+}
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    for(int i = 0;!(i%2);i+=2){
+        cout << "Yo!!!\n";
+    }
+}
