129d9c03e4c2b578e2db160661bfb38e0c9f3ef4
Delphisss <Kong_led_@hotmail.com>
2023-01-10 12:21:04 +0700

    okay

---
+++ 13-4.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+template<class a>
+void mySwap(a &,a &);
+
+int main(){
+	int x, y;
+	string a, b;
+	char p, q;
+	
+	cin >> x >> y >> a >> b >> p >> q;
+	
+	cout << "Before swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	mySwap(x,y);
+	cout << "After swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	mySwap(a,b);
+	cout << "After swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+	mySwap(p,q);
+	cout << "After swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+
+	return 0;
+}
+
+template<class a>
+void mySwap(a &x,a &y){
+	a z = x;
+	x = y;
+	y = z;
+}
+
+//Kong >W<
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,6 +1,2 @@
-   case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }*/
+
+	else if(y > p) cout <<   "|          Yugi wins!!!         |"; // when p is less than y 
