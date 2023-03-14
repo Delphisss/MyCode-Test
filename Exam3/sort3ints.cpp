6d42bcd17c36c0bd9b64b540e180a757adb6e85c
delphiss <Kong_led_@hot.com>
2023-03-14 14:38:59 +0700

    s

---
+++ Exam3/sort3ints.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void sort3ints(int *a ,int *b ,int *c){
+int x,y,z,j,k,l ;
+    *a = *a ;
+    *b = *b ;
+    *c = *c ;
+    x = *a ;
+    y = *b ;
+    z = *c ;
+    if (x < y)
+        {
+            j = x;
+            x = y; 
+            y = j;
+        }
+    if (y < z)
+        {
+            k = y;
+            y = z; 
+            z = k;
+        }
+    if (x < y)
+        {
+            j = x;
+            x = y; 
+            y = j;
+        }
+    *a = x ;
+    *b = y ;
+    *c = z ;
+
+}
+
+
+
+
+int main(){
+
+
+    int a = 1, b = 1, c = 2;
+    sort3ints(&a,&b,&c);
+    printf("%d %d %d",a,b,c);
+
+}
+
+
+
+    /*
+    sort3ints(a,b,c);
+    cout << a << endl << b << endl  << c << endl ;
+
+
+    sort3ints(&a,&b,&c);
+    printf("%d %d %d",a,b,c);
+    */
